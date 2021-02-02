4936e3a64aa954e7a7c3e45c2b3ddae2e465411c
Lane Harris <lrharris215@gmail.com>
2021-02-02 17:32:30 -0500

    did some of the belongsto/hasmany things

--- my_own_active_record/lib/03_associatable.rb
+++ my_own_active_record/lib/03_associatable.rb
@@ -10,22 +10,42 @@ class AssocOptions
   )
 
   def model_class
+    self.class_name.constantize
     # ...
   end
 
   def table_name
+    self.model_class.table_name
     # ...
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+    defaults.keys.each do |key|
+      new_val = options[key] || defaults[key]
+      self.send("#{key}=", new_val)
     # ...
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.capitalize.singularize,
+      :primary_key => :id
+    }
+    defaults.keys.each do |key|
+      new_val = options[key] || defaults[key]
+      self.send("#{key}=", new_val)
+    end
     # ...
   end
 end
@@ -33,6 +53,8 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
+    bt_options = BelongsToOptions.new(name, options)
+
     # ...
   end
 
