a15a0246c982f6b7291c9f1c32ee62f5b2da6bb6
Lane Harris <lrharris215@gmail.com>
2021-02-02 15:58:32 -0500

    #initialize, ::all, ::parse_all, ::finalize

--- my_own_active_record/lib/01_sql_object.rb
+++ my_own_active_record/lib/01_sql_object.rb
@@ -8,27 +8,52 @@ class SQLObject
   
 
   def self.columns
-    # ...
+    return @columns if @columns
+    columns = DBConnection.execute2(<<-SQL).first
+      SELECT * FROM #{self.table_name} 
+    SQL
+    columns.map!{|col| col.to_sym}
+    @columns = columns
   end
 
   def self.finalize!
+    self.columns.each do |col|
+       define_method(col) do
+        self.attributes[col]
+      end
+      define_method("#{col}=") do |new_val|
+        self.attributes[col] = new_val
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
-    @@table_name = table_name
+    @table_name = table_name
     
   end
 
   def self.table_name
-    @@table_name ||= self.name.tableize
+    @table_name ||= self.name.tableize
    
   end
 
   def self.all
+    all_things = DBConnection.execute(<<-SQL)
+    SELECT * FROM #{self.table_name}
+
+    SQL
+    self.parse_all(all_things)
     # ...
   end
 
   def self.parse_all(results)
+    ruby_things = []
+    results.each do |object|
+      ruby_things << self.new(object)
+    end
+    ruby_things
+    
     # ...
   end
 
@@ -37,11 +62,16 @@ class SQLObject
   end
 
   def initialize(params = {})
-    
+    params.each do |key, val|
+      k = key.to_sym
+      raise "unknown attribute '#{k}'" if !self.class.columns.include?(k)
+      self.send("#{k}=", val)
+    end
     # ...
   end
 
   def attributes
+    @attributes ||= {}
     # ...
   end
 
