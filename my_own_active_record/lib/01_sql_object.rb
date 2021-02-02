2d42af3ef23c010940b3b1ed7d43d5de1e9e3519
Lane Harris <lrharris215@gmail.com>
2021-02-02 17:03:35 -0500

    finish sql_object

--- my_own_active_record/lib/01_sql_object.rb
+++ my_own_active_record/lib/01_sql_object.rb
@@ -58,6 +58,11 @@ class SQLObject
   end
 
   def self.find(id)
+    findyfind = DBConnection.execute(<<-SQL, id)
+      SELECT * FROM #{self.table_name} WHERE #{self.table_name}.id = ?
+
+    SQL
+    parse_all(findyfind).first
     # ...
   end
 
@@ -76,18 +81,53 @@ class SQLObject
   end
 
   def attribute_values
+    self.class.columns.map do |attr|
+      self.send(attr)
+    end
     # ...
   end
 
   def insert
+    cols = self.class.columns.drop(1)
+    col_names = cols.map(&:to_s).join(", ")
+
+
+    question_marks = (["?"] * cols.count).join(", ")
+    puts "cols: #{cols}"
+    puts "col_names: #{col_names}"
+    puts "questions: #{question_marks}"
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+
+    INSERT INTO
+      #{self.class.table_name} (#{col_names})
+    VALUES
+      (#{question_marks})
+
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
     # ...
   end
 
   def update
+    set_vals = self.class.columns.map{ |attr| "#{attr} = ?"}.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, self.id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_vals}
+      WHERE
+        id = ?
+      
+      SQL
+    
     # ...
   end
 
   def save
+    insert if self.id.nil?
+    update
     # ...
   end
 end
