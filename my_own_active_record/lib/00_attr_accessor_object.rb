3fac71a24c6320f3ecd0c38c472406cdc65d4ba2
Lane Harris <lrharris215@gmail.com>
2021-02-02 14:25:56 -0500

    create ::my_attr_accessor

--- my_own_active_record/lib/00_attr_accessor_object.rb
+++ my_own_active_record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,20 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name|
+      define_method("#{name}") do
+        instance_variable_get("@#{name}")
+      end
+      define_method("#{name}=") do |new_val|
+        instance_variable_set("@#{name}", new_val)
+      end
+    end
+
+  end
+
+  def initialize
+    @test = ''
   end
 end
+
+
